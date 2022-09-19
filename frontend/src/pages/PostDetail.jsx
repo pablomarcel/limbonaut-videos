e2d250a6e073228a89f69a55b11d770b2a0c91e1
pablomarcel <pablomarcel@outlook.com>
2022-09-18 20:39:18 -0700

    updated PostDetail.jsx

--- frontend/src/pages/PostDetail.jsx
+++ frontend/src/pages/PostDetail.jsx
@@ -93,7 +93,7 @@ const PostDetail = () => {
         </Col>
         <Col>
           <Card.Text>
-            Location: {post.city} - {post.country}
+            Genre: {post.city} - {post.country}
           </Card.Text>
           <Card.Text>
             {post.content}
